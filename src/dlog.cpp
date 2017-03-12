6e4c16057311ca121cf3698f510ac1ea3ff62109
d-likes-it <mailtodetlef@gmail.com>
2017-03-12 21:54:33 +0100

    all compiles and links
    logging itself compiles nicely into args+1 moves

--- src/dlog.cpp
+++ src/dlog.cpp
@@ -1,63 +1,46 @@
 #include <iostream>
+#include <cstring>
 
-#include "loggabletypes.hpp"
+#include "signature.hpp"
 
-constexpr unsigned MAX_ARGS = 8;
+namespace dlog {
 
-template<typename T>
-struct alignas(64) LogLineEntry {
-  uint8_t typeId;
-  T value;
-  LogLineEntry(T v) : typeId(TypeID<T, SupportedTypes>::value), value(v) {};
-  
-  LogLineEntry(LogLineEntry &&) = default;
-  LogLineEntry & operator=(LogLineEntry &&) = default;
-};
+  template <typename Arg>
+  char * copyArg(char * buffer, Arg & t) {
+    static_assert(std::is_trivially_copyable<Arg>::value, "trivially copyable types only");
+    *reinterpret_cast<Arg*>(buffer) = t;
+    return buffer + sizeof(Arg);
+  }
 
-template <typename Arg>
-char * copyArg(char * buffer, Arg & t) {
-  static_assert(std::is_trivially_copyable<Arg>::value, "trivially copyable types only");
-  *reinterpret_cast<LogLineEntry<Arg>*>(buffer) = LogLineEntry<Arg>(t);
-  return buffer + sizeof(LogLineEntry<Arg>);
-}
+  char * copyArgs(char * buffer) {
+    return buffer;
+  }
 
-char * copyArgs(char * buffer) {
-  return buffer;
-}
-
-template <typename Arg, typename... Args>
-char * copyArgs(char * buffer, Arg& arg, Args& ... args) {
-	buffer = copyArg(buffer, arg);
+  template <typename Arg, typename... Args>
+  char * copyArgs(char * buffer, Arg& arg, Args& ... args) {
+    buffer = copyArg(buffer, arg);
     return copyArgs(buffer, args...);
-}
+  }
 
-template <typename... Types>
-constexpr unsigned sizeofArgs(Types &&...) {
-  return sizeof...(Types);
-}
+  template <typename... Args>
+  void writeLog(char const * formatString, Args... args) {
+    typedef signature<Args...> sig;
+    
+    auto const bufSize = sig::sizeOfSignatureString + sig::sizeOfArgs;
+    static_assert(bufSize < 256, "log line too long");
 
-constexpr size_t constStrLength(char const * str){
-  return (*str == 0) ? 0 : constLength(str + 1) + 1;
-}
-
-
-template <typename... Args>
-void writeLog(char const * formatString, Args... args) {
-  static_assert(sizeofArgs(args...) < 9, "too many args");
-  static_assert(sizeof(char const *) + sizeofArgs(args...) * 16 < 256, "log line too long");
-
-  char buffer[MAX_ARGS + sizeof_args(args...) * 16];
-
-  auto len = signature<args>::value::size;
-  std::memcpy(buffer, signature<args>::value::str, len);
-  buffer += len;
+    char buffer[bufSize];
+    std::memcpy(buffer, sig::signatureString, sig::sizeOfSignatureString);
+    // std::memcpy(buffer, Test::signatureString, sig::sizeOfSignatureString);
   
-  char * end = copyArgs(buffer, args...);
-  *end = 0;
-  std::cout << buffer << std::endl;
+    char * end = copyArgs(buffer + sig::sizeOfSignatureString, args...);
+    *end = 0;
+    std::cout << buffer << std::endl;
+  }
+
 }
   
 int main() {
-  writeLog("Hello", 1.0, 2, 3);
+  dlog::writeLog("Hello", 1.0, 2, 3);
 }
 
--- src/signature.hpp
+++ src/signature.hpp
@@ -1,20 +1,34 @@
 #ifndef __DLOG_SIGNATURE__
 #define __DLOG_SIGNATURE__
 
-#include "seq.hpp"
-#include "typeid.hpp"
+#include "type_id.hpp"
 
 namespace dlog {
 
-  template<typename... Args>
-  struct signature {
-    typedef concat_seq<
-      std::tuple<
-	seq<
-	  type_id<Args, Types>::value> ...
-	> > value;
+  template <typename ... Types>
+  struct _sizeof;
+  
+  template <typename TFirst>
+  struct _sizeof<TFirst>  {
+    static const auto value = (sizeof(TFirst));
+  };
+  
+  template <typename TFirst, typename ... TRemaining>
+  struct _sizeof<TFirst, TRemaining ...> {
+    static const auto value = (sizeof(TFirst) + _sizeof<TRemaining...>::value);
+  };
+  
+  template<typename ... Cs> struct signature  {
+    static constexpr std::size_t noOfArgs = sizeof...(Cs);
+    static constexpr std::size_t sizeOfSignatureString = noOfArgs + 1;
+    static constexpr std::size_t sizeOfArgs = _sizeof<Cs...>::value;
+    
+    static constexpr char signatureString[sizeOfSignatureString] = {noOfArgs, type_id<Cs>::value ...};
   };
 
+  // to create the signatureString variable text item for linking... Wow!!!
+  template<typename ... Cs>
+  constexpr char signature<Cs ...>::signatureString[signature<Cs ...>::sizeOfSignatureString];
 }
 
 #endif
